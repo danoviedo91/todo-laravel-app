434b7b48612000908c7ffb2eefb8423af506f94e
danoviedo91 <doviedo@wawand.co>
2019-07-12 16:53:09 -0500

    Remove Font Awesome as our own asset

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -6,7 +6,8 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<meta name="csrf-token" content="{{ csrf_token() }}">
 	<title>Todo List</title>
-	{{ HTML::style(secure_url('css/app.css')) }}
+  {{ HTML::style(secure_url('css/app.css')) }}
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css" integrity="sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ" crossorigin="anonymous">
 </head>
 
   <body>
