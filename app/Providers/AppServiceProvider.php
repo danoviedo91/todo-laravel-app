a64febb8665158cb565a91d26837fba1b5a0c562
danoviedo91 <doviedo@wawand.co>
2019-07-12 17:14:47 -0500

    remove .htaccess since it doesn't work for nginx (duh) and force https routes in AppServiceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -29,5 +30,6 @@ class AppServiceProvider extends ServiceProvider
             $query->bindings;
             $query->time;
         });
+        URL::forceScheme('https');
     }
 }
--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -6,7 +6,7 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<meta name="csrf-token" content="{{ csrf_token() }}">
 	<title>Todo List</title>
-  {{ HTML::style(secure_url('css/app.css')) }}
+  {{ HTML::style('css/app.css') }}
   <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css" integrity="sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ" crossorigin="anonymous">
 </head>
 
@@ -59,7 +59,7 @@
 
 		@yield('content')
 
-    {{ HTML::script(secure_url('js/app.js')) }}
+    {{ HTML::script('js/app.js') }}
     
 </body>
 
